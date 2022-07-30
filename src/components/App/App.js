d7b9d58a2662186fa6620ff011a24b98f79fe275
Motley Engineer <motleyengineer@gmail.com>
2022-07-30 08:58:00 +0100

    refactor to store token in session storage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,8 +5,21 @@ import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  // need to use the optional chaining operator—?.— when accessing the token property
+  // because when you first access the application, the value of sessionStorage.getItem('token') 
+  // will be undefined
+  return userToken?.token
+}
+
 function App() {
-  const [token, setToken] = useState();
+  const token = getToken();
 
   if(!token) {
     return <Login setToken={setToken} />
